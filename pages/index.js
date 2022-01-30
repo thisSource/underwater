59886d1b2872ed85c2bb8c7143c67c1f348dac87
alxSource <alexander.hoglund@gmail.com>
2022-01-30 19:43:59 +0100

    a

--- pages/index.js
+++ pages/index.js
@@ -3,13 +3,6 @@ import Underwater from "../components/Underwater";
 export default function Home() {
   return (
     <div className="text-blue-500 text-5xl font-Salty">
-      <meta
-        name="viewport"
-        content="width=device-width, initial-scale=1.0, maximum-scale=1.0, 
-     user-scalable=0"
-      >
-        {" "}
-      </meta>
       <Underwater />
     </div>
   );
